7af07224cbb2409d185d54881c1c7ce26e590a1c
adminuser <adminuser@debian.(none)>
2015-04-28 04:09:34 +0200

    updated to use argparse library

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Banner grabbing script I hacked up in python. Nothing fancy, but it does the job.
 
-Usage: bannerman.py [host] [method] [uri]
+Usage: bannerman.py [host] [method] [uri] [port]
 [uri] is optional, defaults to /
+[port] is optional, defaults to 80
+
 allowed methods: get, put, connect, trace, head, options
 
-example usage: python bannerman.py www.google.com head /
+example usage: python bannerman.py www.google.com head / 80
--- bannerman.py
+++ bannerman.py
@@ -2,38 +2,36 @@
 
 import sys
 import socket
+import argparse
 
+parser = argparse.ArgumentParser(description ='Send HTTP methods to a URL')
+parser.add_argument('host')
+parser.add_argument('method')
+parser.add_argument('URI', nargs='?', default='/')
+parser.add_argument('port', type=int, nargs='?', default=80)
+args = parser.parse_args()
 
+messages={'get': "GET ", 'options': "OPTIONS ", 'head': "HEAD ", 'trace': "TRACE ", 'put': "PUT ", 'connect': "CONNECT "}
 
-port = 80
-
-if len(sys.argv) < 3:
-	sys.exit('Usage: python bannerman.py [host] [method] (optional)[uri]\r\nAllowed methods: get, options, put, trace, head, connect')
-elif len(sys.argv) == 3:
-	host = sys.argv[1]
-	URI = '/'
-elif len(sys.argv) == 4:
-	host = sys.argv[1]
-	URI = sys.argv[3]
-else:
-	sys.exit('Too many arguements. Usage: python bannerman.py [host]')
-
+try:
+	args.method in messages
+except:
+	print 'Invalid method. Methods are: get, options, head, trace, put, connect'
+	sys.exit()
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
 	print 'Socket creation failed'
 	sys.exit()
 try:
-	ip = socket.gethostbyname(host)
+	ip = socket.gethostbyname(args.host)
 except socket.gaierror:
 	print 'Hostname could not be resolved. Exiting'
 	sys.exit()
 
-s.connect((ip, port))
-endmsg = URI+" HTTP/1.1\r\nHOST: "+host+ "\r\n\r\n"
-messages = {'get': "GET ", 'options': "OPTIONS ", 'head': "HEAD ", 'trace': "TRACE ", 'put': "PUT ", 'connect': "CONNECT "}
-message = messages[sys.argv[2]] + endmsg
-
+s.connect((ip, args.port))
+endmsg = args.URI+" HTTP/1.1\r\nHOST: "+args.host+ "\r\n\r\n"
+message = messages[args.method] + endmsg
 
 try: 
 	s.sendall(message)
@@ -42,6 +40,7 @@ except socket.error:
 	sys.exit()
 
 reply = s.recv(4096)
-
 print reply
+
 s.close()
+
